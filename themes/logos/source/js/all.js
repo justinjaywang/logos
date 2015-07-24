f35da0af8d2633da07eed02548e79746507c1da6
Justin Wang <justinjaywang@gmail.com>
2015-07-24 12:47:19 -0400

    added packery styles with percentage widths

--- themes/logos/source/js/all.js
+++ themes/logos/source/js/all.js
@@ -1,33 +1,50 @@
-+function ($) {
-  'use strict';
+// +function ($) {
+//   'use strict';
 
-  // bind images loaded
-  $('.page-main--index').imagesLoaded()
-    .always( function( instance ) {
-      console.log('all images loaded');
-      pack();
-    })
-    .done( function( instance ) {
-      console.log('all images successfully loaded');
-    })
-    .fail( function() {
-      console.log('all images loaded, at least one is broken');
-    })
-    .progress( function( instance, image ) {
-      var result = image.isLoaded ? 'loaded' : 'broken';
-      console.log( 'image is ' + result + ' for ' + image.img.src );
-    });
-    // TO DO: get rid of this shizz
+//   // bind images loaded
+//   $('.page-main--index').imagesLoaded()
+//     .always( function( instance ) {
+//       console.log('all images loaded');
+//       pack();
+//     })
+//     .done( function( instance ) {
+//       console.log('all images successfully loaded');
+//     })
+//     .fail( function() {
+//       console.log('all images loaded, at least one is broken');
+//     })
+//     .progress( function( instance, image ) {
+//       var result = image.isLoaded ? 'loaded' : 'broken';
+//       console.log( 'image is ' + result + ' for ' + image.img.src );
+//     });
+//     // TO DO: get rid of this shizz
 
-    // initialize packery
-    var pack = function() {
-      var $container = $('.page-main--index');
-      // init
-      $container.packery({
-        itemSelector: '.postPreview',
-        columnWidth: 240,
-        gutter: 20
+//     // initialize packery
+//     var pack = function() {
+//       var $container = $('.page-main--index');
+//       // init
+//       $container.packery({
+//         itemSelector: '.postPreview',
+//         columnWidth: '.column',
+//         percentPosition: true
+//       });
+//     };
+
+// }(jQuery);
+
+(function() {
+  var container;
+
+  container = document.querySelector(".page-main--index");
+
+  if (container) {
+    imagesLoaded(container, function() {
+      var pckry;
+      return pckry = new Packery(container, {
+        itemSelector: ".postPreview",
+        gutter: 0
       });
-    };
+    });
+  }
 
-}(jQuery);
+}).call(this);
