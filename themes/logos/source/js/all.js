3a4cf59a172fcfc841b7e47da632b11d8f99b123
Justin Wang <justinjaywang@gmail.com>
2015-07-23 20:29:29 -0400

    built out theme a little

--- themes/logos/source/js/all.js
+++ themes/logos/source/js/all.js
@@ -1,5 +1,33 @@
 +function ($) {
   'use strict';
 
+  // bind images loaded
+  $('.page-main--index').imagesLoaded()
+    .always( function( instance ) {
+      console.log('all images loaded');
+      pack();
+    })
+    .done( function( instance ) {
+      console.log('all images successfully loaded');
+    })
+    .fail( function() {
+      console.log('all images loaded, at least one is broken');
+    })
+    .progress( function( instance, image ) {
+      var result = image.isLoaded ? 'loaded' : 'broken';
+      console.log( 'image is ' + result + ' for ' + image.img.src );
+    });
+    // TO DO: get rid of this shizz
+
+    // initialize packery
+    var pack = function() {
+      var $container = $('.page-main--index');
+      // init
+      $container.packery({
+        itemSelector: '.postPreview',
+        columnWidth: 240,
+        gutter: 20
+      });
+    };
 
 }(jQuery);
